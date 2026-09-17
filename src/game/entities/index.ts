a7679a279d69b1a565884c393c0e0e0a649fb896
joaoVBRodrigues <joaovictor.brodr@gmail.com>
2026-09-17 12:14:11 -0300

    refactor: add entity exports index

---
+++ src/game/entities/index.ts
@@ -0,0 +1,18 @@
+export type {
+    Transform,
+    Vector2,
+} from './entityTypes';
+
+export type { PlayerEntity } from './player';
+
+export type {
+    EnemyEntity,
+    EnemyType,
+} from './enemy';
+
+export type {
+    ProjectileEntity,
+    ProjectileOwner,
+} from './projectile';
+
+export type { IslandEntity } from './island';
