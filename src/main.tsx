6ea9543f6650613844b4cb9ae975dc6df511a3d3
joaoVBRodrigues <joaovictor.brodr@gmail.com>
2026-09-17 18:28:54 -0300

    refactor: initialize application services

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { worker } from './mocks/browser'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient()
+
+async function bootstrap() {
+  await worker.start({ onUnhandledRequest: 'bypass' })
+
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </StrictMode>,
+  )
+}
+
+void bootstrap()
